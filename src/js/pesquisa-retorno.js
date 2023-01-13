e56a378d9de79a27e86bae9ec033443b9d170bc7
Vinicius <vinicius.vinibola@gmail.com>
2023-01-13 14:06:36 -0300

    fiz algumas melhorias na pesquisa

--- src/js/pesquisa-retorno.js
+++ src/js/pesquisa-retorno.js
@@ -2,13 +2,14 @@ const pesquisado = sessionStorage.getItem('pesquisado');
 fetch('https://api.themoviedb.org/3/search/movie?api_key=a7c8f3e436b89729fc0f9d797ee42c55&query=' + pesquisado)
   .then(response => response.json())
   .then(data => {
-    // obtenha o elemento HTML onde você deseja adicionar os resultados da pesquisa
     const filmeContainer = document.querySelector(".filme-container");
     const movies = data.results;
-    // percorra os resultados da pesquisa
+    let titulo = document.getElementsByTagName("title");
+    
+    titulo[0].text = `Webflix - ${pesquisado}`
     movies.forEach(movie => {
         const infoUrl = `https://www.themoviedb.org/movie/${movie.id}-${movie.title}`;
-
+        
         if(movie.backdrop_path == null){
             movie.backdrop_path = movie.poster_path;
             if(movie.backdrop_path == null){
@@ -16,9 +17,9 @@ fetch('https://api.themoviedb.org/3/search/movie?api_key=a7c8f3e436b89729fc0f9d7
             }
         }
         filmeContainer.innerHTML += `
-      <div class="filme">
-      <a href="${infoUrl}" target="_blank"><img src="${img_url}${movie.backdrop_path}" alt="poster"></a>
-                <p class="titulo-filme">${movie.title}</p>
+        <div class="filme">
+          <a href="${infoUrl}" target="_blank"><img src="${img_url}${movie.backdrop_path}" alt="poster"></a>
+            <p class="titulo-filme">${movie.title}</p>
         </div>
       `
     });
--- src/js/pesquisa.js
+++ src/js/pesquisa.js
@@ -4,6 +4,8 @@ botao.addEventListener('click', () => {
     const pesquisado = document.getElementById("campo_pesquisa").value;
 
     sessionStorage.setItem('pesquisado', pesquisado);
-
+    if(!pesquisado){
+        return;
+    }
     window.location.href = 'pesquisa.html';
 })
