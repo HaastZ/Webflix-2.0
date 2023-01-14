889b6a72ce4370332865607a7da1cc2c4434fb96
Vinicius <vinicius.vinibola@gmail.com>
2023-01-13 23:46:39 -0300

    arrumei algumas imagens que estavam meio erradas e coloquei alguns links nos botoes que estavam faltando

--- src/js/lancamento.js
+++ src/js/lancamento.js
@@ -11,10 +11,16 @@ async function getMovies() {
     for (const movie of movies) {
     const infoUrl = `https://www.themoviedb.org/movie/${movie.id}-${movie.title}`;
 
+      if(movie.backdrop_path == null){
+        movie.backdrop_path = movie.poster_path;
+        if(movie.backdrop_path == null){
+           return;
+        }
+      }
       filmeContainer.innerHTML += `
       <div class="filme">
-      <a href="${infoUrl}" target="_blank"><img src="${img_url}${movie.backdrop_path}" alt="poster"></a>
-                <p class="titulo-filme">${movie.title}</p>
+        <a href="${infoUrl}" target="_blank"><img src="${img_url}${movie.backdrop_path}" alt="poster"></a>
+          <p class="titulo-filme">${movie.title}</p>
         </div>
       `
 
